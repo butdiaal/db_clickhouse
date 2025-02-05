1d343a61de4fa6febc853856203ad4b9caadeeae
butdiaal <dianabutko2007@gmail.com>
2025-02-05 20:31:30 +0300

    commit

--- connect.py
+++ connect.py
@@ -10,21 +10,30 @@ parser.add_argument('-p', '--password', default='', help='Пароль')
 
 args = parser.parse_args()
 
+success = False
+
 try:
     client = Client(host=args.host, port=args.port, user=args.user, password=args.password)
+    print('Подключение успешно.')
 
     try:
         client.execute('''CREATE DATABASE IF NOT EXISTS db_master''')
+        print('База данных успешно создана.')
 
         client.execute('''CREATE TABLE IF NOT EXISTS element
             (doc_id UInt32, 
             centroid Array(Float64)) 
                 ENGINE = MergeTree() 
                 ORDER BY doc_id''')
+        print('Таблица успешно создана.')
+
+        success = True
 
     except Exception as e:
-        print(f"Ошибка создания: {e}")
+        print(f"Ошибка создания: {e}.")
 
 
 except errors.ServerException as e:
-    print(f'Ошибка подключения к ClickHouse: {e}')
+    print(f'Ошибка подключения к ClickHouse: {e}.')
+
+
