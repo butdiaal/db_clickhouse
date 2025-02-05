418f805beffce5070fbf6f21522438ae9113a2a7
butdiaal <dianabutko2007@gmail.com>
2025-02-06 00:40:46 +0300

    commit

--- connect.py
+++ connect.py
@@ -2,22 +2,26 @@ from clickhouse_driver import Client
 import argparse
 
 success = False
+client = None
 
 def main():
     global client
+    global success
     parser = argparse.ArgumentParser(description='Подключение к серверу')
 
     parser.add_argument('--host', default='localhost', help='Хост')
     parser.add_argument('--port', type=int, default=9000, help='Порт')
     parser.add_argument('-u', '--user', default='default', help='Имя пользователя')
     parser.add_argument('-p', '--password', default='', help='Пароль')
+    parser.add_argument('--database', default='db_master', help='Имя базы данных')
+    parser.add_argument('--table', default='element', help='Имя таблицы')
 
     args = parser.parse_args()
 
     try:
         client = Client(host=args.host, port=args.port, user=args.user, password=args.password)
         print('Подключение успешно.')
-        create_db(client)
+        create_db(client, args.database, args.table)
 
     except errors.ServerException as e:
         print(f'Ошибка подключения к ClickHouse: {e}.')
@@ -26,14 +30,18 @@ def main():
 def create_db(client):
     global success
     try:
-        client.execute('''CREATE DATABASE IF NOT EXISTS db_master''')
+        client.execute(f'''CREATE DATABASE IF NOT EXISTS {database_name}''')
         print('База данных успешно создана.')
 
-        client.execute('''CREATE TABLE IF NOT EXISTS element
-            (doc_id UUID, 
-            centroid Array(Float64)) 
-                ENGINE = MergeTree() 
-                ORDER BY doc_id''')
+        client.execute(f'''
+            CREATE TABLE IF NOT EXISTS {table_name}
+            (
+                doc_id UUID,
+                centroid Array(Float64)
+            )
+            ENGINE = MergeTree()
+            ORDER BY doc_id
+        ''')
         print('Таблица успешно создана.')
 
         success = True
