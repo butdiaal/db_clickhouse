664d63cc8499c195bb51d38d60b46ff1565908cc
butdiaal <dianabutko2007@gmail.com>
2025-02-06 11:41:12 +0300

    commit

--- generation.py
+++ generation.py
@@ -4,25 +4,26 @@ import argparse
 import json
 import uuid
 
-output_file_name = None
+parser = argparse.ArgumentParser(description='Генерация')
+
+parser.add_argument('--low', type=float, default=0.0, help='Нижняя граница диапазона')
+parser.add_argument('--high', type=float, default=1.0, help='Верхняя граница диапазона')
+parser.add_argument('--size', type=int, default=512, help='Размер каждого вектора')
+parser.add_argument('--count', type=int, default=20000, help='Количество векторов')
+parser.add_argument('--file_output', type=str, default='elements.json', help='Имя файла для сохранения векторов.')
+
+args = parser.parse_args()
+
+output_file_name = args.file_output
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Генерация')
-
-    parser.add_argument('--low', type=float, default=0.0, help='Нижняя граница диапазона')
-    parser.add_argument('--high', type=float, default=1.0, help='Верхняя граница диапазона')
-    parser.add_argument('--size', type=int, default=512, help='Размер каждого вектора')
-    parser.add_argument('--count', type=int, default=20000, help='Количество векторов')
-    parser.add_argument('--file_output', type=str, default='elements.json', help='Имя файла для сохранения векторов.')
-
-    args = parser.parse_args()
-
-    output_file_name = args.file_output
-
     elements = generate(args)
     save_to_json(elements, output_file_name)
 
     print(f"Векторы успешно сохранены в файл {output_file_name}")
+
+    return output_file_name
 
 
 def generate(args):
