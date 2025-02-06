6f6bb4673d54058a1d66c5a94848b81a7459c0ef
butdiaal <dianabutko2007@gmail.com>
2025-02-06 17:19:13 +0300

    commit

--- connect.py
+++ connect.py
@@ -1,40 +1,60 @@
-from clickhouse_driver import Client, errors
+import logging
 import argparse
+from clickhouse_driver import Client, errors
 
-client = None
-success = False
 
+'''Parses command line arguments. Connections to ClickHouse'''
 def main():
-    global client
-    global success
-    parser = argparse.ArgumentParser(description='Подключение к серверу')
+    parser = argparse.ArgumentParser(description='Connecting to the server')
 
-    parser.add_argument('--host', default='localhost', help='Хост')
-    parser.add_argument('--port', type=int, default=9000, help='Порт')
-    parser.add_argument('-u', '--user', default='default', help='Имя пользователя')
-    parser.add_argument('-p', '--password', default='', help='Пароль')
-    parser.add_argument('--database', default='db_master', help='Имя базы данных')
-    parser.add_argument('--table', default='element', help='Имя таблицы')
+    parser.add_argument('--host', default='localhost', help='Host')
+    parser.add_argument('--port', type=int, default=9000, help='Port')
+    parser.add_argument('-u', '--user', default='default', help='User')
+    parser.add_argument('-p', '--password', default='', help='Password')
+    parser.add_argument('--database', default='db_master', help='Name of the database')
+    parser.add_argument('--table', default='element', help='Table name')
 
     args = parser.parse_args()
 
     try:
         client = Client(host=args.host, port=args.port, user=args.user, password=args.password)
-        print('Подключение успешно.')
-        create_db(client, args.database, args.table)
+        logging.error('Connection successful')
+        check_db(client, args.database, args.table)
 
     except errors.ServerException as e:
-        print(f'Ошибка подключения к ClickHouse: {e}.')
+        logging.error(f'Error connecting to ClickHouse: {e}.')
 
 
+'''Checks if the database and the table in it exist'''
+def check_db(client, database_name, table_name):
+    try:
+        databases = client.execute("SHOW DATABASES")
+        if database_name not in [db[0] for db in databases]:
+            logging.error(f'Database "{database_name}" does not exist, creating it.')
+            create_db(client, database_name, table_name)
+            return False
+
+        tables = client.execute(f"SHOW TABLES FROM {database_name}")
+        if table_name not in [table[0] for table in tables]:
+            logging.error(f'Table "{table_name}" does not exist in database "{database_name}", creating it.')
+            create_db(client, database_name, table_name)
+            return False
+
+        logging.error(f'Database "{database_name}" and table "{table_name}" exist.')
+        return True
+
+    except Exception as e:
+        logging.error(f"Check error: {e}.")
+        return False
+
+'''Creating a database and a table'''
 def create_db(client, database_name, table_name):
-    global success
     try:
         client.execute(f'''CREATE DATABASE IF NOT EXISTS {database_name}''')
-        print('База данных успешно создана.')
+        logging.error('The database has been created successfully')
 
         client.execute(f'''
-            CREATE TABLE IF NOT EXISTS {table_name}
+            CREATE TABLE IF NOT EXISTS {database_name}.{table_name}
             (
                 doc_id UUID,
                 centroid Array(Float64)
@@ -42,14 +62,12 @@ def create_db(client, database_name, table_name):
             ENGINE = MergeTree()
             ORDER BY doc_id
         ''')
-        print('Таблица успешно создана.')
-
-        success = True
+        logging.error('The table was created successfully')
 
     except Exception as e:
-        print(f"Ошибка создания: {e}.")
+        logging.error(f"Creation error: {e}.")
 
-    return success
+
 if __name__ == "__main__":
     main()
 
