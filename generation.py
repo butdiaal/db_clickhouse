8d90b58baab24569735a858cda5e6ae3410639fe
butdiaal <dianabutko2007@gmail.com>
2025-02-06 18:14:04 +0300

    commit

--- generation.py
+++ generation.py
@@ -1,31 +1,30 @@
 import random
 import numpy as np
+import logging
 import argparse
 import json
 import uuid
 
-parser = argparse.ArgumentParser(description='Генерация')
 
-parser.add_argument('--low', type=float, default=0.0, help='Нижняя граница диапазона')
-parser.add_argument('--high', type=float, default=1.0, help='Верхняя граница диапазона')
-parser.add_argument('--size', type=int, default=512, help='Размер каждого вектора')
-parser.add_argument('--count', type=int, default=20000, help='Количество векторов')
-parser.add_argument('--file_output', type=str, default='elements.json', help='Имя файла для сохранения векторов.')
-
-args = parser.parse_args()
-
-output_file_name = args.file_output
-
-
+'''The main function of the program processes command line arguments, generates vectors and saves them to a file.'''
 def main():
+    parser = argparse.ArgumentParser(description='Generation')
+
+    parser.add_argument('--low', type=float, default=0.0, help='The lower limit of the range')
+    parser.add_argument('--high', type=float, default=1.0, help='Upper limit of the range')
+    parser.add_argument('--size', type=int, default=512, help='The size of each vector')
+    parser.add_argument('--count', type=int, default=20000, help='Number of vectors')
+    parser.add_argument('--file_output', type=str, default='elements.json', help='The name of the file for saving vectors')
+
+    args = parser.parse_args()
+
     elements = generate(args)
-    save_to_json(elements, output_file_name)
+    save_to_json(elements, args.file_output)
 
-    print(f"Векторы успешно сохранены в файл {output_file_name}")
-
-    return output_file_name
+    logging.debug(f"Vectors have been successfully saved to a file {args.file_output}")
 
 
+'''Generates vectors based on the specified parameters'''
 def generate(args):
     existing_uuids = set()
 
@@ -47,8 +46,9 @@ def generate(args):
     return elements
 
 
-def save_to_json(elements, output_file_name):
-    with open(output_file_name, 'w') as json_file:
+'''Saves data to a file in JSON format'''
+def save_to_json(elements, file_output):
+    with open(file_output, 'w') as json_file:
         json.dump(elements, json_file, indent=4)
 
 
